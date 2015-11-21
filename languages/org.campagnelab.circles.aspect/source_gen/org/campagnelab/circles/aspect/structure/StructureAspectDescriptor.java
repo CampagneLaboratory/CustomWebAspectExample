c9a27646a82fc97cc97ae65fbf2fd68358022811
Fabien Campagne <fac2003@campagnelab.org>
2015-11-21 09:58:45 -0500

    Add mockup.editor under dependencies of web facet (needed to make references to editors of the mockup language inside the web facet). Create WebEditors root node under the web facet and list some editors of the mockup language.

--- languages/org.campagnelab.circles.aspect/source_gen/org/campagnelab/circles/aspect/structure/StructureAspectDescriptor.java
+++ languages/org.campagnelab.circles.aspect/source_gen/org/campagnelab/circles/aspect/structure/StructureAspectDescriptor.java
@@ -14,35 +14,41 @@ public class StructureAspectDescriptor extends BaseStructureAspectDescriptor {
 
   /*package*/ final ConceptDescriptor myConceptContributeConcepts = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.ContributeConcepts", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL)).super_("jetbrains.mps.lang.core.structure.BaseConcept").super_(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).parents("jetbrains.mps.lang.core.structure.BaseConcept", "org.campagnelab.circles.aspect.structure.IIncludeConcepts").parentIds(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL), MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d610de2L)).abstract_().create();
   /*package*/ final ConceptDescriptor myConceptDatabase = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.Database", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x2970c96b0e706973L)).super_("jetbrains.mps.lang.core.structure.BaseConcept").super_(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).parents("jetbrains.mps.lang.core.structure.BaseConcept").parentIds(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).propertyDescriptors(new ConceptDescriptorBuilder.Prop(2986108014587570548L, "url"), new ConceptDescriptorBuilder.Prop(4824635715685243887L, "user"), new ConceptDescriptorBuilder.Prop(4824635715685243890L, "password")).properties("url", "user", "password").create();
+  /*package*/ final ConceptDescriptor myConceptEditorRef = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.EditorRef", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x15998bfb2b1eb1ecL)).super_("jetbrains.mps.lang.core.structure.BaseConcept").super_(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).parents("jetbrains.mps.lang.core.structure.BaseConcept").parentIds(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).referenceDescriptors(new ConceptDescriptorBuilder.Ref(1556429057109701101L, "editor", MetaIdFactory.conceptId(0x18bc659203a64e29L, 0xa83a7ff23bde13baL, 0xf9845363abL), false)).references("editor").create();
   /*package*/ final ConceptDescriptor myConceptIIncludeConcepts = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.IIncludeConcepts", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d610de2L)).interface_().create();
   /*package*/ final ConceptDescriptor myConceptIncludeConcepts = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.IncludeConcepts", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x42f48c9f1414cf69L)).super_("org.campagnelab.circles.aspect.structure.ContributeConcepts").super_(MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL)).parents("org.campagnelab.circles.aspect.structure.ContributeConcepts", "org.campagnelab.circles.aspect.structure.IIncludeConcepts").parentIds(MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL), MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d610de2L)).referenceDescriptors(new ConceptDescriptorBuilder.Ref(4824635715685240682L, "c", MetaIdFactory.conceptId(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL), false)).references("c").alias("include", "Include Concepts For Persistence").create();
   /*package*/ final ConceptDescriptor myConceptIncludeLanguage = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.IncludeLanguage", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d611395L)).super_("org.campagnelab.circles.aspect.structure.ContributeConcepts").super_(MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL)).parents("org.campagnelab.circles.aspect.structure.ContributeConcepts", "org.campagnelab.circles.aspect.structure.IIncludeConcepts").parentIds(MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL), MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d610de2L)).referenceDescriptors(new ConceptDescriptorBuilder.Ref(1004539053072257952L, "language", MetaIdFactory.conceptId(0x7866978ea0f04cc7L, 0x81bc4d213d9375e1L, 0x312abca18ab8c8c0L), true)).references("language").create();
   /*package*/ final ConceptDescriptor myConceptPersistedConcepts = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.PersistedConcepts", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x42f48c9f1414cf63L)).super_("jetbrains.mps.lang.core.structure.BaseConcept").super_(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).parents("jetbrains.mps.lang.core.structure.BaseConcept").parentIds(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).childDescriptors(new ConceptDescriptorBuilder.Link(4824635715685240684L, "inclusionRules", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0xdf0d6f24d62582cL), true, true, false)).children(new String[]{"inclusionRules"}, new boolean[]{true}).create();
+  /*package*/ final ConceptDescriptor myConceptWebEditors = new ConceptDescriptorBuilder("org.campagnelab.circles.aspect.structure.WebEditors", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x15998bfb2b1eb1ebL)).super_("jetbrains.mps.lang.core.structure.BaseConcept").super_(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).parents("jetbrains.mps.lang.core.structure.BaseConcept").parentIds(MetaIdFactory.conceptId(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x10802efe25aL)).childDescriptors(new ConceptDescriptorBuilder.Link(1556429057109701121L, "editors", MetaIdFactory.conceptId(0x8c40f9f4b3bd42d0L, 0x8b658e644273493cL, 0x15998bfb2b1eb1ecL), true, true, false)).children(new String[]{"editors"}, new boolean[]{true}).alias("", "Set of Editors to Web Enable").create();
 
   @Override
   public Collection<ConceptDescriptor> getDescriptors() {
-    return Arrays.asList(myConceptContributeConcepts, myConceptDatabase, myConceptIIncludeConcepts, myConceptIncludeConcepts, myConceptIncludeLanguage, myConceptPersistedConcepts);
+    return Arrays.asList(myConceptContributeConcepts, myConceptDatabase, myConceptEditorRef, myConceptIIncludeConcepts, myConceptIncludeConcepts, myConceptIncludeLanguage, myConceptPersistedConcepts, myConceptWebEditors);
   }
 
   @Override
   @Nullable
   public ConceptDescriptor getDescriptor(String conceptFqName) {
-    switch (Arrays.binarySearch(stringSwitchCases_1htk8d_a0a0k, conceptFqName)) {
+    switch (Arrays.binarySearch(stringSwitchCases_1htk8d_a0a0m, conceptFqName)) {
       case 0:
         return myConceptContributeConcepts;
       case 1:
         return myConceptDatabase;
       case 2:
-        return myConceptIIncludeConcepts;
+        return myConceptEditorRef;
       case 3:
-        return myConceptIncludeConcepts;
+        return myConceptIIncludeConcepts;
       case 4:
-        return myConceptIncludeLanguage;
+        return myConceptIncludeConcepts;
       case 5:
+        return myConceptIncludeLanguage;
+      case 6:
         return myConceptPersistedConcepts;
+      case 7:
+        return myConceptWebEditors;
       default:
         return null;
     }
   }
-  private static String[] stringSwitchCases_1htk8d_a0a0k = new String[]{"org.campagnelab.circles.aspect.structure.ContributeConcepts", "org.campagnelab.circles.aspect.structure.Database", "org.campagnelab.circles.aspect.structure.IIncludeConcepts", "org.campagnelab.circles.aspect.structure.IncludeConcepts", "org.campagnelab.circles.aspect.structure.IncludeLanguage", "org.campagnelab.circles.aspect.structure.PersistedConcepts"};
+  private static String[] stringSwitchCases_1htk8d_a0a0m = new String[]{"org.campagnelab.circles.aspect.structure.ContributeConcepts", "org.campagnelab.circles.aspect.structure.Database", "org.campagnelab.circles.aspect.structure.EditorRef", "org.campagnelab.circles.aspect.structure.IIncludeConcepts", "org.campagnelab.circles.aspect.structure.IncludeConcepts", "org.campagnelab.circles.aspect.structure.IncludeLanguage", "org.campagnelab.circles.aspect.structure.PersistedConcepts", "org.campagnelab.circles.aspect.structure.WebEditors"};
 }
--- languages/org.campagnelab.circles.mockup/languageModels/web.mps
+++ languages/org.campagnelab.circles.mockup/languageModels/web.mps
@@ -8,7 +8,7 @@
     <devkit ref="fbc25dd2-5da4-483a-8b19-70928e1b62d7(jetbrains.mps.devkit.general-purpose)" />
   </languages>
   <imports>
-    <import index="64sm" ref="8202d3eb-bfe0-4fb5-9544-c67c5faa531d/java:com.orientechnologies.orient.core(org.campagnelab.circles.persistence/)" />
+    <import index="lnem" ref="r:c03de3d9-fc26-4342-a7f1-effa193be9df(org.campagnelab.circles.mockup.editor)" />
     <import index="avse" ref="r:51fcf803-02a7-4ac6-994f-040e45f3e9a5(org.campagnelab.circles.mockup.structure)" implicit="true" />
   </imports>
   <registry>
@@ -17,6 +17,12 @@
         <property id="2986108014587570548" name="url" index="B0a0v" />
         <property id="4824635715685243890" name="password" index="1mS5tH" />
         <property id="4824635715685243887" name="user" index="1mS5tK" />
+      </concept>
+      <concept id="1556429057109701099" name="org.campagnelab.circles.aspect.structure.WebEditors" flags="ng" index="YxNCg">
+        <child id="1556429057109701121" name="editors" index="YxNBU" />
+      </concept>
+      <concept id="1556429057109701100" name="org.campagnelab.circles.aspect.structure.EditorRef" flags="ng" index="YxNCn">
+        <reference id="1556429057109701101" name="editor" index="YxNCm" />
       </concept>
       <concept id="4824635715685240681" name="org.campagnelab.circles.aspect.structure.IncludeConcepts" flags="ng" index="1mS4fQ">
         <reference id="4824635715685240682" name="c" index="1mS4fP" />
@@ -46,6 +52,20 @@
     </node>
     <node concept="1mS4fQ" id="3Uieg3Ml$wl" role="1mS4fN">
       <ref role="1mS4fP" to="avse:7pelyIRohq0" resolve="FileItem" />
+    </node>
+  </node>
+  <node concept="YxNCg" id="1mpyZGF7YyP">
+    <node concept="YxNCn" id="1mpyZGF7YyZ" role="YxNBU">
+      <ref role="YxNCm" to="lnem:7pelyIRkxD_" resolve="CircleContainer_Editor" />
+    </node>
+    <node concept="YxNCn" id="1mpyZGF7Yz1" role="YxNBU">
+      <ref role="YxNCm" to="lnem:7pelyIRny2r" resolve="CircleRef_Editor" />
+    </node>
+    <node concept="YxNCn" id="1mpyZGF7Yz4" role="YxNBU">
+      <ref role="YxNCm" to="lnem:7pelyIRkzwA" resolve="Circle_Editor" />
+    </node>
+    <node concept="YxNCn" id="1mpyZGF7Yz8" role="YxNBU">
+      <ref role="YxNCm" to="lnem:7pelyIRkzxh" resolve="Dashboard_Editor" />
     </node>
   </node>
 </model>
